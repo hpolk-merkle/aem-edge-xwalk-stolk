e37c3ce76b47d2772f0a581df82e9318019dec4a
s-polk <steven.polk@merkle.com>
2024-11-20 13:37:34 -0500

    list js test

--- blocks/list/list.js
+++ blocks/list/list.js
@@ -2,6 +2,16 @@ export default function decorate(block) {
   const [listWrapper] = block.children;
 
   const blockquote = document.createElement('blocklist');
+  // let urlParams = new URLSearchParams({
+  //   offset: 0,
+  // });
+  const url = '/spreadsheet.json';
+
+  fetch(url, { method: 'GET' })
+    .then((Result) => Result.json())
+    .then((results) => {
+      console.log(results);
+    });
   blockquote.textContent = listWrapper.textContent.trim();
   listWrapper.replaceChildren(blockquote);
 }
