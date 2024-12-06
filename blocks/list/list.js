0a5d22b80343603c6ab2ca09d898fa21d061d894
s-polk <steven.polk@merkle.com>
2024-12-06 10:51:38 -0500

    update list js

--- blocks/list/list.js
+++ blocks/list/list.js
@@ -9,39 +9,40 @@
 // }
 
 export default async function decorate(block) {
-  const [listWrapper] = block.children;
-
-  const blockList = document.createElement('ul');
-  blockList.classList.add('list-block');
-  // let urlParams = new URLSearchParams({
-  //   offset: 0,
-  // });
-  const url = '/spreadsheet.json';
-
-  // const json = await req(url);
-  const response = await fetch(url);
-  const testResponse = await fetch('/content/aemcloudondentsu/us/en/jcr%3Acontent.txt');
-
-  if (testResponse.ok) {
-    const test = await testResponse.text();
-
-    console.log(test);
-  }
-
-  if (response.ok) {
-    const json = await response.json();
-
-    console.log(json);
-
-    json.data.forEach((item) => {
-      const listItem = document.createElement('li');
-      listItem.classList.add('list-block-item');
-      listItem.textContent = item.Path;
-
-      blockList.appendChild(listItem);
-    });
-  }
-
-  // blockList.textContent = listWrapper.textContent.trim();
-  listWrapper.replaceChildren(blockList);
+  console.log(block);
+  // const [listWrapper] = block.children;
+  //
+  // const blockList = document.createElement('ul');
+  // blockList.classList.add('list-block');
+  // // let urlParams = new URLSearchParams({
+  // //   offset: 0,
+  // // });
+  // const url = '/spreadsheet.json';
+  //
+  // // const json = await req(url);
+  // const response = await fetch(url);
+  // const testResponse = await fetch('/content/aemcloudondentsu/us/en/jcr%3Acontent.txt');
+  //
+  // if (testResponse.ok) {
+  //   const test = await testResponse.text();
+  //
+  //   console.log(test);
+  // }
+  //
+  // if (response.ok) {
+  //   const json = await response.json();
+  //
+  //   console.log(json);
+  //
+  //   json.data.forEach((item) => {
+  //     const listItem = document.createElement('li');
+  //     listItem.classList.add('list-block-item');
+  //     listItem.textContent = item.Path;
+  //
+  //     blockList.appendChild(listItem);
+  //   });
+  // }
+  //
+  // // blockList.textContent = listWrapper.textContent.trim();
+  // listWrapper.replaceChildren(blockList);
 }
